45219de0a4aaa240df3edf29eedd8df032db34ab
Omer Ali Adali <omadali@k2m01s02.42kocaeli.com.tr>
2025-11-27 00:57:00 +0300

    ex00 finish

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,9 +1,18 @@
 #include <cstring>
+#include <iostream>
 
 int main(int argc,char **argv)
 {
-	for(int i = 0,i<std::strlen(argv),i++)
+	if(argc == 1)
 	{
-		
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return(0);
+	}	
+	for(int i = 1;i < argc;i++)
+	{
+		for(int b = 0;argv[i][b] != '\0';b++)
+			std::cout << (char)std::toupper(argv[i][b]);
 	}
+	std::cout << std::endl;
+	return(0);
 }
