399300ac794d2df046d6e17ec944aa4746f32dea
Omer Ali Adali <omadali@k2m01s02.42kocaeli.com.tr>
2025-11-27 00:46:53 +0300

    rebuild

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,21 +1,9 @@
-#include <iostream>
-#include <cctype>
+#include <cstring>
 
-int main(int argc, char **argv)
+int main(int argc,char **argv)
 {
-	if(argc ==1)
+	for(int i = 0,i<std::strlen(argv),i++)
 	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return (0);
+		
 	}
-	
-	for (int i= 1; i < argc; i++)
-	{
-		for(int j = 0;argv[i][j]; j++)
-		{ 
-			std::cout << (char)std::toupper(argv[i][j]);
-		}
-	}
-	std::cout << std::endl;
-	return (0);
 }
